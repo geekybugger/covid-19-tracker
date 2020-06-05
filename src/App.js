991efa609944866573168fe10ad473fbdbf2ca93
Krishnakumar V <krishkumarv2001@gmail.com>
2020-06-05 23:40:56 +0530

    Added a AppBar with menu function[NOT OPTIMISED]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Cards, Charts, CountryPicker} from './components';
+import {Cards, Charts, CountryPicker, Appbar} from './components';
 
 import styles from './App.module.css';
 import {fetchData} from './api';
@@ -17,10 +17,14 @@ class App extends React.Component{
 
     render(){
         return(
-            <div className={styles.container}>
-                <Cards data = {this.state.data} />
-                <CountryPicker />
-                <Charts />
+            <div>  
+                <Appbar />
+                <div className={styles.container}>
+
+                    <Cards data = {this.state.data} />
+                    <CountryPicker />
+                    <Charts />
+                </div>
             </div>
         )
     }
---
+++ src/components/Appbar/Appbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styles from './Appbar.module.css';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const Appbar = () => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return(
+        <div className={styles.root}>
+            <AppBar position="fixed">
+                <Toolbar>
+                <IconButton edge="start" className={styles.menuButton} color="inherit" aria-label="menu" onClick={handleClick} >
+                    <MenuIcon />
+                </IconButton>
+
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >          
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
+
+                <Typography variant="h6" className={styles.title}>
+                    News
+                </Typography>
+                <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+export default Appbar;
